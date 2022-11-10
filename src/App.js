bdfc8cc62caab11c2a1758bde1e0eb7743e1eb83
leeji0805 <96579569+leeji0805@users.noreply.github.com>
2022-11-11 01:39:31 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+function ImageUploadExample() {
+
+  
+  const proverbs=["100%","95%","90%","80%"]
+  
+  const getRandom = (length) => {
+    
+    return parseInt(Math.random() * length);
+    
+    }     
+  //파일 미리볼 url을 저장해줄 state
+  
+  const [fileImage, setFileImage] = useState("");
+  this.state={
+    prov: "호감도는?"
+  }
+   
+
+  // 파일 저장
+  const saveFileImage = (e) => {
+    setFileImage(URL.createObjectURL(e.target.files[0]));
+    this.setState({
+      prov : proverbs[getRandom(proverbs.length)]
+    }) 
+  };
+
+  // 파일 삭제
+  const deleteFileImage = () => {
+    URL.revokeObjectURL(fileImage);
+    setFileImage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div class="container">
+        <div class="header">
+            <div id="button">
+                
+            </div>
+        </div>
+        <div class="content">
+            <div class = "leftbox">
+                <div>
+                    {fileImage && (
+                      <img
+                        alt="sample"
+                        src={fileImage}
+                        style={{ margin: "auto" }}
+                      />
+                    )}
+                    <div
+                      style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <input
+                        name="imgUpload"
+                        type="file"
+                        accept="image/*"
+                        onChange={saveFileImage}
+                      />
+    
+                      <button
+                        style={{
+                          backgroundColor: "gray",
+                          color: "white",
+                          width: "55px",
+                          height: "40px",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => deleteFileImage()}
+                      >
+                        삭제
+                      </button>
+                    </div>
+                  </div>
+            </div>
+            <div class="rightbox">
+                <div class = "inerbox">
+                    <div class = "upperbox">
+                        <div id="pertext">
+                        {state=""}
+                        </div>
+                    </div>
+                    <div id="downbox">
+                        {this.state}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+      
+    </>
   );
 }
 
-export default App;
+export default ImageUploadExample;
